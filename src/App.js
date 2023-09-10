c6243b1e42eb675a520214399b53b7fbc0b7e98a
Padmavathi <Gowtham.Mutta1@T-Mobile.com>
2023-09-10 15:09:06 +0530

    Add .gitignore file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './Carousel.css';
+import React from "react";
+import nature1 from './assets/nature1.jpg'
+import nature2 from './assets/nature2.jpg'
+import nature3 from './assets/nature3.jpg'
+import nature4 from './assets/nature4.jpg'
+import nature5 from './assets/nature5.jpg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className='parent'>
+    <main className="main-container">
+      <section>
+        <img src={nature1} />
+        <img src={nature2} />
+        <img src={nature3} />
+        <img src={nature4} />
+        <img src={nature5} />
+      </section>
+    </main>
+</div>
   );
 }
 
